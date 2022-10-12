b1fb3f087db22cbefbec6964bf03a3cf712ddbdd
nokdoot <nokdoot@naver.com>
2022-10-12 09:15:00 +0900

    adding dapp

---
+++ dapp/src/lib/Wallet.svelte
@@ -0,0 +1,13 @@
+<script>
+  const connect = () => {
+    console.log(typeof window.klaytn !== 'undefined');
+    klaytn.enable();
+    klaytn.on('accountsChanged', function(accounts) {
+      console.log(accounts);
+    })
+  }
+</script>
+
+<button on:click="{connect}">
+  connect
+</button>
