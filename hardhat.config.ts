cfa3966eb6a9554766aa071e05c87c17704a82d0
nokdoot <nokdoot@naver.com>
2022-10-18 01:54:56 +0900

    chore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,6 +18,15 @@ const config: HardhatUserConfig = {
       chainId: 1001,
       // url: 'https://api.baobab.klaytn.net:8651',
       url: 'https://public-node-api.klaytnapi.com/v1/baobab',
+    },
+    cypress: {
+      accounts: [
+        process.env.PRIVATE_KEY as string,
+        process.env.TEST_ADDRESS1_PRIVATE_KEY as string,
+        process.env.TEST_ADDRESS2_PRIVATE_KEY as string
+      ],
+      chainId: 8217,
+      url: 'https://klaytn01.fandom.finance/',
     }
   }
 };
